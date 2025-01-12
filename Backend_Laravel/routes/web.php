02a55a3e8406e99b2e4d2819580090a2df316c27
SunStudio <mahsunmuh0@gmail.com>
2025-01-12 15:53:40 +0700

    update BE

--- Backend_Laravel/routes/web.php
+++ Backend_Laravel/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landingPage');
 });
+
+Route::get('login', [UserController::class, 'index'])->name('login');
+Route::get('dashboard', [UserController::class, 'dashboard'])->name('dashboard');
